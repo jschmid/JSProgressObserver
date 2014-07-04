da6b3e37504ebb55b10f59bfedd47d5ec7e60c2d
Jonas Schmid <jonas.schmid@gmail.com>
2014-07-04 16:36:56 +0200

    Removed failing tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -9,34 +9,13 @@
 SPEC_BEGIN(InitialTests)
 
 describe(@"My initial tests", ^{
+    context(@"will pass", ^{
 
-  context(@"will fail", ^{
+        it(@"can do maths",
+           ^{ [[theValue(1) should] beLessThan:theValue(23)]; });
 
-      it(@"can do maths", ^{
-          [[theValue(1) should] equal:theValue(2)];
-      });
-
-      it(@"can read", ^{
-          [[@"number" should] equal:@"string"];
-      });
-    
-      it(@"will wait and fail", ^{
-          NSObject *object = [[NSObject alloc] init];
-          [[object shouldEventually] receive:@selector(autoContentAccessingProxy)];
-      });
-  });
-
-  context(@"will pass", ^{
-    
-      it(@"can do maths", ^{
-        [[theValue(1) should] beLessThan:theValue(23)];
-      });
-    
-      it(@"can read", ^{
-          [[@"team" shouldNot] containString:@"I"];
-      });  
-  });
-  
+        it(@"can read", ^{ [[@"team" shouldNot] containString:@"I"]; });
+    });
 });
 
 SPEC_END
